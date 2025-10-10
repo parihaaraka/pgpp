fdf6579802cb4b11925112e8fb7b60190690f819
parihaaraka <parihaaraka@gmail.com>
2025-10-10 13:11:26 +0300

    half step to make it human-friendly

--- include/pgpp/result.h
+++ include/pgpp/result.h
@@ -4,7 +4,10 @@
 #ifndef PG_RESULT_H
 #define PG_RESULT_H
 
+#include <optional>
+#include <charconv>
 #include <string>
+#include <system_error>
 #include <vector>
 #include <memory>
 #if __has_include(<postgresql/libpq-fe.h>)
@@ -50,6 +53,45 @@ public:
     const char* column_name(int col_number) const noexcept;
     unsigned int column_type(int col_number) const noexcept;
     int scale(int col_number) const noexcept;
+
+    template <typename T>
+    std::optional<T> value(int row, int col) const
+    {
+        if (is_null(row, col))
+            return {};
+        auto val = std::string_view(raw_value(row, col), value_size(row, col));
+        if constexpr (std::is_integral_v<T>  || std::is_floating_point_v<T>)
+        {
+            T num;
+            auto ec = std::from_chars(val.data(), val.data() + val.size(), num).ec;
+            if (ec == std::errc{})
+                return {num};
+            throw std::runtime_error(std::make_error_code(ec).message());
+        }
+        if constexpr (std::is_same_v<T, std::string_view>)
+        {
+            return {val};
+        }
+        if constexpr (std::is_same_v<T, std::string>)
+        {
+            return {std::string{val}};
+        }
+    }
+
+    template <typename T>
+    std::optional<T> value(int row, const char* col_name) const
+    {
+        int cind = column_index(col_name);
+        if (cind < 0)
+            throw std::runtime_error("column `" + std::string(col_name) + "` not found");
+        return value<T>(row, cind);
+    }
+
+    template <typename T>
+    std::optional<T> value(int row, std::string_view col_name) const
+    {
+        return value<T>(row, col_name.data());
+    }
 
     operator bool() const;
     bool copy_in_ready() const;
